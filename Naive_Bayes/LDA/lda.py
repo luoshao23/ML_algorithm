92c41ac8d89ce4bfcb8a442640acd6e3866cedd0
luoshao23 <luoshao23@gmail.com>
2018-08-15 10:12:20 +0800

    Latent Dirichlet Allocation

---
+++ Naive_Bayes/LDA/lda.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class LDA(object):
+
+    def __init__(sef):
+        pass
+
+    def fit(self, X, y=None):
+        pass
+
+    def transform(self, X, y=None):
+        pass
+
+    def fit_transform(self, X, y=None):
+        pass
+
+    def _fit(self, X):
+        pass
