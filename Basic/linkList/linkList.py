22d8bae5f45b23f25b225499799fffd30a6b91df
luoshao23 <luoshao23@gmail.com>
2018-11-19 21:49:43 +0800

    link list

--- Basic/linkList/linkList.py
+++ Basic/linkList/linkList.py
@@ -40,12 +40,12 @@ class SingleLinkedList(object):
 
     def travel(self):
         this = self._head
-        slow_this = None
-        while this:
-            slow_this = this
-            this = this.next
-
-        return slow_this
+        if self.is_empty():
+            return this
+        else:
+            while this.next:
+                this = this.next
+            return this
 
     def add(self, item):
         """add to the top of the list"""
@@ -55,44 +55,102 @@ class SingleLinkedList(object):
     def append(self, item):
         """append to the tail of the list"""
         this = self.travel()
-        if hasattr(this, 'next'):
-            this.next = item
-        else:
+        if this is None:
             self._head = item
+        else:
+            this.next = item
 
     def insert(self, pos, item):
-        if pos > self.length:
+        if pos > self.length or pos < 0:
             raise IndexError("index of bound!")
-        count = 0
-        this = self._head
-        slow_this = None
-        while count < pos:
-            slow_this = this
-            this = this.next
-            count += 1
-
-        if hasattr(slow_this, 'next'):
-            slow_this.next = item
+        elif pos == 0:
+            self.add(item)
         else:
-            self._head = item
-        item.next = this
+            count = 0
+            this = self._head
+            while count < pos - 1:
+                this = this.next
+                count += 1
+            item.next = this.next
+            this.next = item
+
+    def search(self, item):
+        if not self.is_empty():
+            this = self._head
+            while this.next:
+                if this.value == item.value:
+                    return True
+                this = this.next
+        return False
+
+    def remove(self, pos):
+        if not self.is_empty():
+            if pos > 0 and pos < self.length:
+                count = 0
+                this = self._head
+                while count < pos - 1:
+                    this = this.next
+                    count += 1
+                tmp = this.next
+                this.next = tmp.next
+                del tmp
+            elif pos == 0:
+                tmp = self._head
+                self._head = tmp.next
+                del tmp
+            else:
+                raise IndexError("Out of bound.")
+        else:
+            raise RuntimeError("Cannot remove item from an empty list.")
+
+
+
+    # def insert(self, pos, item):
+    #     if pos > self.length:
+    #         raise IndexError("index of bound!")
+    #     count = 0
+    #     this = self._head
+    #     slow_this = None
+    #     while count < pos:
+    #         slow_this = this
+    #         this = this.next
+    #         count += 1
+    #     if self.is_empty():
+    #         self._head = item
+    #     else:
+    #         slow_this.next = item
+    #     item.next = this
 
 
 if __name__ == "__main__":
     ll = SingleLinkedList()
+    print("is empty?: ", ll.is_empty())
     for i in range(5):
         n = Node(i)
         ll.add(n)
     print(ll)
+    print("is empty?: ", ll.is_empty())
     print(ll.length)
 
     # ll = SingleLinkedList()
     # ll.append(Node(5))
-    # print(ll)
     # print(ll.length)
-
+    ll.insert(0, Node(999))
+    print(ll)
     ll.insert(5, Node(999))
     print(ll)
+    ll.insert(ll.length, Node(999))
+    print(ll)
     print(ll.length)
+    print(ll.search(Node(0)))
+    print(ll.search(Node(10)))
+    print(SingleLinkedList().search(Node(10)))
+    ll.remove(2)
+    print(ll)
+    ll.remove(0)
+    print(ll)
+    ll.remove(5)
+    print(ll)
+    ll.remove(5)
 
 
