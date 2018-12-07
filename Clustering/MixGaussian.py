8cdd48c47dd81863136d9d2c33e1f771eccc6a48
luoshao23 <luoshao23@gmail.com>
2018-12-07 15:31:27 +0800

    add

--- Clustering/MixGaussian.py
+++ Clustering/MixGaussian.py
@@ -1,27 +1,44 @@
 import numpy as np
 from scipy.stats import multivariate_normal
 from sklearn.mixture import GaussianMixture
+from sklearn.cluster import k_means
 
 
 class MixGaussion(object):
-    def __init__(self, n_componments=1, max_iter=100):
+    def __init__(self, n_componments=1, init_method='kmeans', max_iter=100):
         self.n_componments = n_componments
+        self.init_method = init_method
         self.max_iter = max_iter
 
-    def fit(self, X, y=None):
-        '''
+    def _initialize(self, X):
+        n_componments = self.n_componments
+        n_samples, n_features = X.shape
+        weights = np.ones(n_componments) / n_componments
+        if self.init_method == 'kmeans':
+            means = k_means(X, n_clusters=n_componments)[0]
+        else:
+            means = X[np.random.choice(n_samples, n_componments, replace=False)]
+        covs = [np.eye(n_features) for _ in range(n_componments)]
+
+        return n_componments, n_samples, n_features, weights, means, covs
+
+    def fit_predict(self, X, y=None):
+        """
+        Parameters
+        ------
         X: (n_samples, n_features)
         weights: (n_componments, )
         means: (n_componments, n_features)
         covs: (n_componments, n_features, n_features)
         posterior: (n_samples, n_componments)
         pms: (n_samples, n_componments)
-        '''
-        n_componments = self.n_componments
-        m, d = X.shape
-        weights = np.ones(n_componments) / n_componments
-        means = X[np.random.choice(m, n_componments, replace=False)]
-        covs = [0.1 * np.eye(d) for _ in range(n_componments)]
+        """
+
+        n_componments, n_samples, n_features, weights, means, covs = self._initialize(
+            X)
+        self.weights = weights
+        self.means = means
+        self.covs = covs
 
         for _ in range(self.max_iter):
             pms = np.stack([multivariate_normal.pdf(X, mean=means[k], cov=covs[k]) for k in range(n_componments)], axis=1)
@@ -31,11 +48,21 @@ class MixGaussion(object):
             means /= posterior.sum(0, keepdims=True).T
             covs = [np.dot(
                 (X - means[k]).T, (X - means[k])*posterior[:, k][:, np.newaxis])/posterior[:, k].sum() for k in range(n_componments)]
-            weights = posterior.sum(0) / m
+            weights = posterior.sum(0) / n_samples
 
         res = posterior.argmax(1)
 
         return res
+
+    def fit(self, X, y=None):
+        self.fit_predict(X, y)
+        return self
+
+    def predict(self, X):
+        pms = np.stack([multivariate_normal.pdf(X, mean=self.means[k], cov=self.covs[k])
+                        for k in range(self.n_componments)], axis=1)
+        posterior = self.weights * pms
+        return posterior.argmax(1)
 
 
 if __name__ == "__main__":
@@ -44,15 +71,13 @@ if __name__ == "__main__":
     X3 = np.random.multivariate_normal([11, 11], [[11, 1], [1, 11]], 10)
     X = np.concatenate([X1, X2, X3])
 
-    mg = MixGaussion(3, 300)
-    res = mg.fit(X)
-    print(res)
+    mg = MixGaussion(3, 100)
+    mg.fit(X)
+    print('Total', mg.predict(X))
+    print('X3', mg.predict(X3))
 
     gm = GaussianMixture(3)
     gm.fit(X)
-    res = gm.predict(X2)
-    print(res)
-
-
-
+    print('Total', gm.predict(X))
+    print('X3', gm.predict(X3))
 
