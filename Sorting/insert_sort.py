404cf23195cba6be1fa2fdb7d089d907a9a2b3c8
luoshao23 <luoshao23@gmail.com>
2019-03-17 11:57:19 +0800

    sort

--- Sorting/insert_sort.py
+++ Sorting/insert_sort.py
@@ -1,3 +1,5 @@
+import time
+
 def insert_sort(lst):
     """
     best: O(n)
@@ -12,8 +14,25 @@ def insert_sort(lst):
             else:
                 break
 
+
+def insert_sort_better(lst):
+    """
+    best: O(n)
+    worst: O(n^2)
+    stable
+    """
+    for i in range(1, len(lst)):
+        cur = lst[i]
+        j = i
+        while j > 0 and lst[j - 1] > cur:
+            lst[j] = lst[j - 1]
+            j -= 1
+        lst[j] = cur
+
+
 if __name__ == "__main__":
-    lst = [5, 4, 7, 1, 12, 13, 23, 0, 4]
-    print(lst)
-    insert_sort(lst)
-    print(lst)
+    st = time.time()
+    for _ in range(10000):
+        lst = [5, 4, 7, 1, 12, 13, 23, 0, 4]
+        insert_sort_better(lst)
+    print(time.time() - st)
